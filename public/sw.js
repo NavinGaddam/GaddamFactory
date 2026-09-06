53c4cc655fe985e3493a21ea1a34243032613eea
NavinGaddam <navingaddam2@gmail.com>
2026-09-06 21:54:31 +0530

    feat: complete Gaddam Factory project foundation and branding

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE='gaddam-factory-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.json','/icon.svg']))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
